d589de2ca2bbb8dc8e13ee13954cbbb0387a5abb
satyananda mandal <satyanandamandal5566@gmail.com>
2025-08-08 12:28:55 +0000

    conversion of data type

---
+++ 01_first/03_conversionooperation.js
@@ -0,0 +1,21 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof score);
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let islogedIn = 1 
+
+let booleanIsLoggedIn = Boolean(islogedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//"" => false
+// "hitesh" => true
+
