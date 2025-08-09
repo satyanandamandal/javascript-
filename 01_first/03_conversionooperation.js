deee1cd49a0f31f72831095a52889acdf0af690f
satyananda mandal <satyanandamandal5566@gmail.com>
2025-08-09 05:37:16 +0000

    conversion

--- 01_first/03_conversionooperation.js
+++ 01_first/03_conversionooperation.js
@@ -1,10 +1,10 @@
 let score = 33
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof score);
+//console.log(typeof score);
 
 //"33" => 33
 //"33abc" => NaN
@@ -13,9 +13,31 @@ console.log(typeof score);
 let islogedIn = 1 
 
 let booleanIsLoggedIn = Boolean(islogedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 //"" => false
 // "hitesh" => true
+
+//*************************operation************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
 
